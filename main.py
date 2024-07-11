7be096ee2c6081d1460f8b21b38f41e771afdfa8
Romina Lima Martinelli <limamartinellirominanatalia@gmail.com>
2024-07-11 13:57:06 -0300

    Initial commit

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
-import os
 import telebot
 from telebot import types
-from flask import Flask
-from waitress import serve  # Importa Waitress
+import requests
+import time
 
 # Conexión con nuestro BOT
-TOKEN = 'your_telegram_bot_token'
+TOKEN = '6782919923:AAGyiXLvnUEnBwPUye8TvQDQbi1qqoBkEX0'
+
 bot = telebot.TeleBot(TOKEN)
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "Hello, World!"
 
 # Manejador del comando /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     markup = types.ReplyKeyboardMarkup(row_width=2)
-    btn1 = types.KeyboardButton('Consultas')
-    btn2 = types.KeyboardButton('Metricas')
+    btn1 = types.KeyboardButton('Tengo una consulta')
+    btn2 = types.KeyboardButton('Gestionar mis métricas')
     btn3 = types.KeyboardButton('Accesos directos')
-    btn4 = types.KeyboardButton('Tengo una sugerencia')
+    btn4 = types.KeyboardButton('Hacer una sugerencia')
     btn5 = types.KeyboardButton('Quiero postular mi llamada')
-    markup.add(btn1, btn2, btn3, btn4, btn5)
+    btn6 = types.KeyboardButton('Volver atrás')
+    markup.add(btn1, btn2, btn3, btn4, btn5, btn6)
     
     bot.send_message(message.chat.id, "Bienvenido! Selecciona una opción:", reply_markup=markup)
 
@@ -35,25 +30,37 @@ def send_help(message):
 # Manejador de mensajes
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    if message.text == 'Consultas':
-        markup = types.InlineKeyboardMarkup(row_width=2)
-        btn_si = types.InlineKeyboardButton('Si', callback_data='Consultas_si')
-        btn_no = types.InlineKeyboardButton('No', callback_data='Consultas_no')
-        btn_back = types.InlineKeyboardButton('Volver atrás', callback_data='back')
-        markup.add(btn_si, btn_no, btn_back)
-        bot.send_message(message.chat.id, "¿Te ayudo con tu llamada?", reply_markup=markup)
-    elif message.text == 'Metricas':
-        bot.reply_to(message, "Has seleccionado Métricas")
+    if message.text == 'Tengo una consulta':
+        handle_consultas(message)
+    elif message.text == 'Gestionar mis métricas':
+        bot.send_message(message.chat.id, "Gestiona tus métricas aquí: [Gestionar mis métricas](https://santirojo.github.io/calculamsk/)", parse_mode="Markdown")
     elif message.text == 'Accesos directos':
-        bot.reply_to(message, "Has seleccionado Accesos directos")
-    elif message.text == 'Tengo una sugerencia':
-        bot.reply_to(message, "Has seleccionado Tengo una sugerencia")
+        handle_accesos_directos(message)
+    elif message.text == 'Hacer una sugerencia':
+        handle_sugerencias(message)
     elif message.text == 'Quiero postular mi llamada':
-        bot.reply_to(message, "Has seleccionado Quiero postular mi llamada")
+        handle_postular_llamada(message)
     elif message.text == 'Volver atrás':
         send_welcome(message)
     else:
         bot.reply_to(message, "Opción no válida, por favor selecciona una opción del menú.")
+
+def handle_consultas(message):
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    btn_si = types.InlineKeyboardButton('Si', callback_data='Consultas_si')
+    btn_no = types.InlineKeyboardButton('No', callback_data='Consultas_no')
+    btn_back = types.InlineKeyboardButton('Volver atrás', callback_data='back')
+    markup.add(btn_si, btn_no, btn_back)
+    bot.send_message(message.chat.id, "¿Te ayudo con tu llamada?", reply_markup=markup)
+
+def handle_accesos_directos(message):
+    bot.send_message(message.chat.id, "Accesos directos:\n1. Google: https://www.google.com\n2. GitHub: https://www.github.com")
+
+def handle_sugerencias(message):
+    bot.send_message(message.chat.id, "Por favor, envía tu sugerencia y la revisaremos pronto.")
+
+def handle_postular_llamada(message):
+    bot.send_message(message.chat.id, "Para postular tu llamada, por favor sigue las instrucciones:\n1. Describe tu llamada.\n2. Envía los detalles relevantes.")
 
 # Manejador de consultas (Inline Keyboard)
 @bot.callback_query_handler(func=lambda call: True)
@@ -67,6 +74,16 @@ def callback_query(call):
         send_welcome(call.message)
 
 if __name__ == "__main__":
-    # Render define el puerto en la variable de entorno PORT
-    port = int(os.environ.get('PORT', 5000))
-    serve(app, host='0.0.0.0', port=port)  # Usa Waitress para servir tu aplicación
+    while True:
+        try:
+            bot.polling(none_stop=True, timeout=40, long_polling_timeout=20)
+        except requests.exceptions.ReadTimeout:
+            print("ReadTimeout: La solicitud a la API de Telegram ha superado el tiempo de espera.")
+            time.sleep(15)  # Espera 15 segundos antes de intentar nuevamente
+        except Exception as e:
+            print(f"Ha ocurrido un error inesperado: {e}")
+            time.sleep(15)  # Espera 15 segundos antes de intentar nuevamente
+
+
+
+
