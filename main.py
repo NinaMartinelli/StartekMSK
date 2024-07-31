f2d95ba3476374445e0930c5490502cf7d461391
Romina Lima Martinelli <limamartinellirominanatalia@gmail.com>
2024-07-30 22:20:53 -0300

    Prueba conexión

--- main.py
+++ main.py
@@ -15,50 +15,51 @@ if TOKEN is None:
 
 # Inicializa el bot
 bot = telebot.TeleBot(TOKEN)
-# URL de la API para obtener el permiso
-PERMISSION_API_URL = "https://apimocha.com/nominamsk/nomina"
 
-# Función para solicitar permiso
-def request_permission():
+# Función para solicitar permiso a la API
+def request_permission(user_id):
+    url = "https://apimocha.com/nominamsk/nomina"
+    payload = {'user_id': user_id}
+    headers = {'Content-Type': 'application/json'}
+    
     try:
-        response = requests.get(PERMISSION_API_URL)
-        if response.status_code == 200:
-            data = response.json()
-            return data.get("permitido", False)
-        else:
-            return False
+        response = requests.post(url, json=payload, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        return data.get('has_permission', False)  # Asume que la API devuelve un campo 'has_permission'
     except requests.RequestException as e:
-        print(f"Error al conectar con la API: {e}")
+        print(f"Error al solicitar permiso: {e}")
         return False
 
 # Función para preguntar cómo se siente el usuario
 def ask_how_are_you(message):
     markup = types.ReplyKeyboardMarkup(row_width=3, one_time_keyboard=True)
-    joy = types.KeyboardButton('😃')
-    sadness = types.KeyboardButton('😢')
-    neutral = types.KeyboardButton('😐')
-    fear = types.KeyboardButton('😱')
-    anger = types.KeyboardButton('😡')
+    joy = types.KeyboardButton('😃 Alegre')
+    sadness = types.KeyboardButton('😢 Tristeza')
+    neutral = types.KeyboardButton('😐 Indiferente')
+    fear = types.KeyboardButton('😱 Miedo')
+    anger = types.KeyboardButton('😡 Enojo')
     markup.add(joy, sadness, neutral, fear, anger)
     bot.send_message(message.chat.id, "¿Cómo te sientes hoy?", reply_markup=markup)
 
 # Manejador del comando /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    if request_permission():
+    user_id = message.from_user.id
+    if request_permission(user_id):
         ask_how_are_you(message)
     else:
         bot.send_message(message.chat.id, "No tienes permiso para usar este bot.")
 
 # Manejador de las respuestas a la pregunta "¿Cómo te sientes hoy?"
-@bot.message_handler(func=lambda message: message.text in ['😃', '😢', '😐', '😱', '😡'])
+@bot.message_handler(func=lambda message: message.text in ['😃 Alegre', '😢 Tristeza', '😐 Indiferente', '😱 Miedo', '😡 Enojo'])
 def handle_feelings(message):
     feeling_responses = {
-        '😃': '¡Me alegra saber que te sientes feliz!',
-        '😢': 'Lo siento, espero que te sientas mejor pronto.',
-        '😐': 'Entiendo, todos tenemos días neutrales. ¡Vamos a intentar un cambio de actitud!',
-        '😱': 'Debe ser difícil sentir miedo, pero confía en tus acciones y tendrás buen desempeño.',
-        '😡': 'Intenta cambiar esa energía con pensamientos positivos y buscando la solución o alternativa que tengas para brindar.'
+        '😃 Alegre': '¡Me alegra saber que te sientes feliz!',
+        '😢 Tristeza': 'Lo siento, espero que te sientas mejor pronto.',
+        '😐 Indiferente': 'Entiendo, todos tenemos días que no sentimos.Vamos a intentar un cambio de Actitud!',
+        '😱 Miedo': 'Debe ser difícil sentir miedo. Pero confia en tus acciones y tendras buen desempeño',
+        '😡 Enojo': 'Intenta cambiar esa energía con pensamientos positivos y buscando la solucion o alternativa que tengas para brindar'
     }
     response = feeling_responses.get(message.text, "Gracias por compartir cómo te sientes.")
     bot.send_message(message.chat.id, response)
@@ -82,7 +83,7 @@ def handle_consultas(message):
     markup = types.ReplyKeyboardMarkup(row_width=1)
     btn1 = types.KeyboardButton('¿Cómo cargo un ajuste?')
     btn2 = types.KeyboardButton('¿Como cargo un ajuste de una línea cancelada?')
-    btn3 = types.KeyboardButton('¿Cómo cargo ajuste de una línea cancelada si no me sale el NIM en pec o no tiene otra cuenta activa?')
+    btn3 = types.KeyboardButton('¿Cómo cargo ajuste de una línea cancelada si no me sale el NIM en pec o no tiene otra cuenta activa ?')
     btn4 = types.KeyboardButton('¿Cómo solicitar soporte técnico?')
     btn_back = types.KeyboardButton('Atrás')
     markup.add(btn1, btn2, btn3, btn4, btn_back)
@@ -99,16 +100,15 @@ def handle_consultas(message):
 def handle_specific_consultas(message):
     responses = {
         '¿Cómo cargo un ajuste?':'Todos los ajustes se cargan por PEC, tenés que validar el motivo que genera el ajuste para determinar si corresponde o no. Podes ver motivos en https://claroaup.sharepoint.com/sites/ClaroPedia2/SitePages/Instructivos/Ajustes---Multiskill.aspx',
-        '¿Cómo cargo un ajuste de una línea cancelada?': 'Siempre deben cargarse a través de PEC utilizando el número de cuenta o alguna línea activa o cancelada con número no borrado. Recordar aclarar dentro del pedido los datos de la cuenta a ajustar.',
+        '¿Cuál cargo un ajuste de linea CANCELADA?': 'Siempre deben cargarse a través de PEC utilizando el número de cuenta o alguna línea activa o cancelada con número no borrado. Recordar aclarar dentro del pedido los datos de la cuenta a ajustar.',
         '¿Cómo cargo ajuste de una línea cancelada si no me sale el NIM en pec o no tiene otra cuenta activa?': 'Lo debes cargar a través de SMAX- SOLICITUDES CALL CENTER - LOYALTY.',
-        'Cambio de sim - FRAUDE': """Cliente inicia la conversación diciendo que se quedó sin línea de golpe o recibió un SMS o mail informando que se hizo un cambio de SIM y desconoce.
-        SMS: “Pediste un nuevo chip Claro para tu línea y será entregado en breve. Si vos no lo solicitaste contáctanos en claro.com.ar/chatonline” y no solicitó un cambio de sim.
-        Informale que por su seguridad vas a suspender la línea.
-        VALIDA DNI.
-        SUSPENDE X ROBO.
-        CARGA TICKLER DESIM.
-        GESTIONA ENVIO CHIP."""
-    }
+        'Cambio de sim - FRAUDE':"""Cliente inicia la conversación diciendo que se quedó sin línea de golpe o recibió un SMS o mail informando que se hizo un cambio de SIM y desconoce.
+SMS: “Pediste un nuevo chip Claro para tu línea y será entregado en breve. Si vos no lo solicitaste contáctanos en claro.com.ar/chatonline” y no solicitó un cambio de sim.
+Informale que por su seguridad vas a suspender la línea.
+VALIDA DNI.
+SUSPENDE X ROBO.
+CARGA TICKLER DESIM.
+GESTIONA ENVIO CHIP."""}
     response = responses.get(message.text, "Consulta no reconocida.")
     bot.send_message(message.chat.id, response)
 
@@ -163,7 +163,6 @@ def shutdown_bot(signum, frame):
 
 signal.signal(signal.SIGINT, shutdown_bot)
 signal.signal(signal.SIGTERM, shutdown_bot)
-
 if __name__ == "__main__":
     while True:
         try:
@@ -174,8 +173,11 @@ if __name__ == "__main__":
         except requests.exceptions.ConnectionError:
             print("ConnectionError: Problema de conexión.")
             time.sleep(15)  # Espera 15 segundos antes de intentar nuevamente
+        except telebot.apihelper.ApiTelegramException as e:
+            print(f"Error de API Telegram: {e}")
+            time.sleep(15)  # Espera 15 segundos antes de intentar nuevamente
         except Exception as e:
             print(f"Ha ocurrido un error inesperado: {e}")
             time.sleep(15)  # Espera 15 segundos antes de intentar nuevamente
 
-            
+
